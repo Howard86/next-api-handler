06c50039180bc4da95b2e9f4f9137a51b9a0beb7
Howard Tai <howard@howardism.dev>
2023-03-19 00:25:29 +0000

    chore: update website assets and meta

--- apps/doc/src/pages/_document.tsx
+++ apps/doc/src/pages/_document.tsx
@@ -43,6 +43,27 @@ export default function Document() {
     <Html className="antialiased [font-feature-settings:'ss01']" lang="en">
       <Head>
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#334155" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body className="bg-white dark:bg-slate-900">
         <Main />
